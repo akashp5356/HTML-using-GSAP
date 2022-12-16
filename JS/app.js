dac6259517db1015aa62924c139262cabe72dbb5
akashp <akashp@mkcl.org>
2022-12-16 14:55:05 +0530

    Added GSAP
    Gallery with GSAP

---
+++ JS/app.js
@@ -0,0 +1,167 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".pic1", {
+    x: 400,
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".pic1",
+        start: 3000,
+        end:3700,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".text1", {
+    x: -400,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".text1",
+        start: 3000,
+        end:3700,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".text2", {
+    x: 350,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".text2",
+        start: 3300,
+        end:4000,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".pic2", {
+    x: -250,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".pic2",
+        start: 3300,
+        end:4000,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".pic3", {
+    x: 400,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".pic3",
+        start: 3600,
+        end:4300,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".text3", {
+    x: -400,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".text3",
+        start: 3600,
+        end:4300,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".text4", {
+    x: 350,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".text4",
+        start: 3900,
+        end:4600,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".pic4", {
+    x: -250,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".pic4",
+        start: 3900,
+        end:4600,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+gsap.to(".pic5", {
+    x: 400,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".pic5",
+        start: 4200,
+        end:4900,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".text5", {
+    x: -400,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".text5",
+        start: 4200,
+        end:4900,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".text6", {
+    x: 350,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".text6",
+        start: 4500,
+        end:5200,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
+
+gsap.to(".pic6", {
+    x: -250,
+
+    duration: 0.1,
+    scrollTrigger: {
+        trigger: ".pic6",
+        start: 4500,
+        end:5200,
+        markers:false,
+        scrub:1,
+        toggleActions:"restart none reverse none"
+    }
+})
